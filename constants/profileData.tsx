36c4fe2fc211fb7d4928d7238b13ad132420a688
Dinesh Dhanji <dhanjeehartik@gmail.com>
2024-11-16 09:54:40 +0500

    Update the Expo SDK to 52 (latest).

---
+++ constants/profileData.tsx
@@ -0,0 +1,7 @@
+const profileData = {
+  name: "John Doe",
+  status: "Online",
+  profileImage:
+    "https://i.pinimg.com/564x/da/1d/b1/da1db1452129ce8557f08c8983403752.jpg",
+};
+export { profileData };
